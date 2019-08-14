9324ce3b80f8b7060b7296add78bff30b4042687
jarari <hazardhax11@gmail.com>
2019-08-15 04:47:31 +0900

    More laziness

--- immersive_impact/WatcherTemplate.h
+++ immersive_impact/WatcherTemplate.h
@@ -1,47 +1,49 @@
 #pragma once
-#include <string>
 
 //THIS IS NOT A TEMPLATE CLASS/PARENT CLASS FOR WATCHERS
 //I'm just a fucking lazy bitch so I decided to make this one for copy pasta.
 //Copy & Paste this template for additional watchers.
 
+#include <string>
 #include "SKSE/GameEvents.h"
+#define EVENT 
+#define NAME 
 
-class WatcherTemplate : public BSTEventSink<EVENT> {
+class NAME : public BSTEventSink<EVENT> {
 protected:
-	static WatcherTemplate *instance;
+	static NAME* instance;
 	static std::string className;
 public:
-	WatcherTemplate() {
+	NAME() {
 		if (instance)
 			delete(instance);
 		instance = this;
 		_MESSAGE((className + std::string(" instance created.")).c_str());
 	}
 
-	WatcherTemplate *GetInstance() {
+	static NAME* GetInstance() {
 		return instance;
 	}
-	virtual ~WatcherTemplate();
+	virtual ~NAME();
 
 	static void InitHook();
 
 	static void ResetHook();
 
-	virtual EventResult ReceiveEvent(EVENT *evn, EventDispatcher<EVENT>* src) override;
+	virtual EventResult ReceiveEvent(EVENT* evn, EventDispatcher<EVENT>* src) override;
 };
 
-WatcherTemplate *WatcherTemplate::instance = nullptr;
-std::string WatcherTemplate::className = "WatcherTemplate";
+NAME* NAME::instance = nullptr;
+std::string NAME::className = "";
 
-void WatcherTemplate::InitHook() {
+void NAME::InitHook() {
 	if (instance)
 		delete(instance);
-	instance = new WatcherTemplate();
-	g_questStageEventSource.AddEventSink((BSTEventSink<>*)instance);
+	instance = new NAME();
+	g_cellFullyLoadedEventSource.AddEventSink((BSTEventSink<EVENT>*)instance);
 	_MESSAGE((className + std::string(" added to the sink.")).c_str());
 }
 
-EventResult WatcherTemplate::ReceiveEvent(*evn, EventDispatcher<>* src) {
+EventResult NAME::ReceiveEvent(EVENT* evn, EventDispatcher<EVENT>* src) {
 	return kEvent_Continue;
 }
