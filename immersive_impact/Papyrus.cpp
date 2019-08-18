7948d1b934e9a9a5051b9a6754bd23689df5b277
jarari <hazardhax11@gmail.com>
2019-08-18 18:25:57 +0900

    New configs

--- immersive_impact/Papyrus.cpp
+++ immersive_impact/Papyrus.cpp
@@ -15,6 +15,7 @@ namespace BingleImmersiveImpact {
 	float chargeMul = 1.0f;
 	bool customizedL = false;
 	bool customizedR = false;
+	bool speedAdjustmentEnabled = true;
 	//Since BSFixedStrings can't be compared with char types, we declare them.
 	BSFixedString s_as("attackStop");
 	BSFixedString s_pre("preHitFrame");
@@ -28,33 +29,33 @@ namespace BingleImmersiveImpact {
 	void ModifyAttackSpeedByTypes(TESObjectWEAP* wep, int weptype, bool right) {
 		//If the weapon is 2 handed
 		if (weptype == 5 || weptype == 6) {
-			ActorModifier::ModifyAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_Swing2h] + speedValues[ConfigType::Speed_Offset]);
-			ActorModifier::ModifyAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_Swing2h] + speedValues[ConfigType::Speed_LeftOffset]);
+			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_Swing2h] + speedValues[ConfigType::Speed_Offset]);
+			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_Swing2h] + speedValues[ConfigType::Speed_LeftOffset]);
 			//_MESSAGE("2h %f", speedValues[ConfigType::Speed_Swing2h]);
 		}
 
 		//If the weapon is 1 handed
 		else if (weptype == 1 || weptype == 3 || weptype == 4) {
 			if(right)
-				ActorModifier::ModifyAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_Swing1h] + speedValues[ConfigType::Speed_Offset]);
+				ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_Swing1h] + speedValues[ConfigType::Speed_Offset]);
 			else
-				ActorModifier::ModifyAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_Swing1h] + speedValues[ConfigType::Speed_LeftOffset]);
+				ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_Swing1h] + speedValues[ConfigType::Speed_LeftOffset]);
 			//_MESSAGE("1h %f", speedValues[ConfigType::Speed_Swing1h]);
 		}
 
 		//If the weapon is a dagger
 		else if (weptype == 2) {
 			if(right)
-				ActorModifier::ModifyAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_SwingDag] + speedValues[ConfigType::Speed_Offset]);
+				ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_SwingDag] + speedValues[ConfigType::Speed_Offset]);
 			else
-				ActorModifier::ModifyAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_SwingDag] + speedValues[ConfigType::Speed_LeftOffset]);
+				ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_SwingDag] + speedValues[ConfigType::Speed_LeftOffset]);
 			//_MESSAGE("dag %f", speedValues[ConfigType::Speed_SwingDag]);
 		}
 
 		//Bare hands!
 		else if (weptype == 0) {
-			ActorModifier::ModifyAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_SwingFist] + speedValues[ConfigType::Speed_Offset]);
-			ActorModifier::ModifyAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_SwingFist] + speedValues[ConfigType::Speed_LeftOffset]);
+			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_SwingFist] + speedValues[ConfigType::Speed_Offset]);
+			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_SwingFist] + speedValues[ConfigType::Speed_LeftOffset]);
 			//_MESSAGE("fist %f", speedValues[ConfigType::Speed_SwingFist]);
 		}
 	}
@@ -64,6 +65,16 @@ namespace BingleImmersiveImpact {
 			customizedL = b;
 		else
 			customizedR = b;
+	}
+
+	void EnableSpeedAdjustment(bool b) {
+		if (speedAdjustmentEnabled && !b) {
+			ActorModifier::SetBaseAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", 1);
+			ActorModifier::SetBaseAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", 1);
+			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", 1);
+			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", 1);
+		}
+		speedAdjustmentEnabled = b;
 	}
 
 	float GetDefault(ConfigType c) {
@@ -88,7 +99,40 @@ namespace BingleImmersiveImpact {
 				return 1.0f;
 			case ConfigType::HitFeedback:
 				return 0;
+			case ConfigType::SpeedAdjustment:
+				return 1;
+			case ConfigType::DeflectChanceMul:
+				return 0.1f;
+			case ConfigType::DeflectChanceMax:
+				return 50.0f;
+			case ConfigType::StaggerResetTime:
+				return 2.0f;
+			case ConfigType::StaggerLimit:
+				return 3.0f;
+			case ConfigType::StaggerDamageMax:
+				return 200.0f;
+			case ConfigType::StaggerAny:
+				return 0;
 		}
+	}
+
+	void UpdateFromConfig(ConfigType configtype, float v) {
+		if (configtype < ConfigType::RestrainMovement)
+			speedValues[configtype] = v;
+		else if (configtype == ConfigType::ChargeMul)
+			chargeMul = v;
+		else if (configtype == ConfigType::DeflectChanceMul)
+			HitFeedback::deflectChanceMul = v;
+		else if (configtype == ConfigType::DeflectChanceMax)
+			HitFeedback::deflectChanceMax = v;
+		else if (configtype == ConfigType::StaggerResetTime)
+			HitFeedback::staggerResetTime = v;
+		else if (configtype == ConfigType::StaggerLimit)
+			HitFeedback::staggerLimit = v;
+		else if (configtype == ConfigType::StaggerDamageMax)
+			HitFeedback::staggerDamageMax = v;
+		else if (configtype == ConfigType::StaggerAny)
+			HitFeedback::staggerAny = v;
 	}
 
 
@@ -100,35 +144,42 @@ namespace BingleImmersiveImpact {
 		//_MESSAGE("%s", event);
 
 		if (event == s_as) {
+			if (!speedAdjustmentEnabled)
+				return;
 			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_Pre] + speedValues[ConfigType::Speed_Offset]);
 			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_Pre] + speedValues[ConfigType::Speed_LeftOffset]);
 		}
 
 		//If the event is preHitFrame
 		else if (event == s_pre) {
-			//Calculate Offset values to override any player.modav commands
-			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", 1);
-			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", 1);
-			speedValues[ConfigType::Speed_Offset] = 1 - ActorModifier::GetAV((Actor*)(*g_thePlayer), "WeaponSpeedMult");
-			speedValues[ConfigType::Speed_LeftOffset] = 1 - ActorModifier::GetAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult");
+			if (speedAdjustmentEnabled) {
+				//Calculate Offset values to override any player.modav commands
+				ActorModifier::SetBaseAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", 1);
+				ActorModifier::SetBaseAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", 1);
+				ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", 1);
+				ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", 1);
+				speedValues[ConfigType::Speed_Offset] = 1 - ActorModifier::GetAV((Actor*)(*g_thePlayer), "WeaponSpeedMult");
+				speedValues[ConfigType::Speed_LeftOffset] = 1 - ActorModifier::GetAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult");
 
-			tList<ActiveEffect>* list_ae = ((Actor*)(*g_thePlayer))->magicTarget.GetActiveEffects();
-			tList<ActiveEffect>::Iterator it = list_ae->Begin();
-			while (!it.End()) {
-				ActiveEffect* ae = it.Get();
-				//Instead of looking for specific for ids, look for what actorvalue does this ActiveEffect change.
-				if (ae->actorValue == LookupActorValueByName("WeaponSpeedMult")) {
-					speedValues[ConfigType::Speed_Offset] += ae->magnitude - 1;
-					speedValues[ConfigType::Speed_LeftOffset] += ae->magnitude - 1;
+				tList<ActiveEffect>* list_ae = ((Actor*)(*g_thePlayer))->magicTarget.GetActiveEffects();
+				tList<ActiveEffect>::Iterator it = list_ae->Begin();
+				while (!it.End()) {
+					ActiveEffect* ae = it.Get();
+					//Instead of looking for specific for ids, look for what actorvalue does this ActiveEffect change.
+					if (ae->actorValue == LookupActorValueByName("WeaponSpeedMult")) {
+						speedValues[ConfigType::Speed_Offset] += ae->magnitude - 1;
+						speedValues[ConfigType::Speed_LeftOffset] += ae->magnitude - 1;
+					}
+					++it;
 				}
-				++it;
+
+				ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_Pre] + speedValues[ConfigType::Speed_Offset]);
+				ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_Pre] + speedValues[ConfigType::Speed_LeftOffset]);
 			}
+
 			//Tried OnActorAction, but it sucks. I figured this way is more reliable.
 			ActorModifier::RestrainMovement((Actor*)(*g_thePlayer), true);
 			ActorModifier::RestrainView((Actor*)(*g_thePlayer), false);
-
-			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_Pre] + speedValues[ConfigType::Speed_Offset]);
-			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_Pre] + speedValues[ConfigType::Speed_LeftOffset]);
 
 			float wepReach = EquipWatcher::isTwoHanded ? ((TESObjectWEAP*)(Actor*)(*g_thePlayer)->GetEquippedObject(false))->reach() : 1.0f;
 			float minRange = max((*g_thePlayer)->race->data.handReach, 60.0f);
@@ -138,35 +189,39 @@ namespace BingleImmersiveImpact {
 
 		else if (event == s_sw || event == s_lsw) {
 			ActorModifier::RestrainView((Actor*)(*g_thePlayer), true);
-			//If the event is weaponSwing
-			if (event == s_sw) {
-				if (customizedR) {
-					ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_CustomR_Swing] + speedValues[ConfigType::Speed_Offset]);
+			if (speedAdjustmentEnabled) {
+				//If the event is weaponSwing
+				if (event == s_sw) {
+					if (customizedR) {
+						ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_CustomR_Swing] + speedValues[ConfigType::Speed_Offset]);
+					}
+					else if ((Actor*)(*g_thePlayer)->GetEquippedObject(false)) {
+						TESObjectWEAP* wep = ((TESObjectWEAP*)(Actor*)(*g_thePlayer)->GetEquippedObject(false));
+						int weptype = wep->type();
+						ModifyAttackSpeedByTypes(wep, weptype, true);
+					}
 				}
-				else if ((Actor*)(*g_thePlayer)->GetEquippedObject(false)) {
-					TESObjectWEAP* wep = ((TESObjectWEAP*)(Actor*)(*g_thePlayer)->GetEquippedObject(false));
-					int weptype = wep->type();
-					ModifyAttackSpeedByTypes(wep, weptype, true);
-				}
-			}
-			//If the event is weaponLeftSwing
-			else if (event == s_lsw) {
-				if (customizedL) {
-					ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_CustomL_Swing] + speedValues[ConfigType::Speed_LeftOffset]);
-				}
-				else if ((Actor*)(*g_thePlayer)->GetEquippedObject(true)) {
-					TESObjectWEAP* wep = ((TESObjectWEAP*)(Actor*)(*g_thePlayer)->GetEquippedObject(true));
-					int weptype = wep->type();
-					ModifyAttackSpeedByTypes(wep, weptype, false);
+				//If the event is weaponLeftSwing
+				else if (event == s_lsw) {
+					if (customizedL) {
+						ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_CustomL_Swing] + speedValues[ConfigType::Speed_LeftOffset]);
+					}
+					else if ((Actor*)(*g_thePlayer)->GetEquippedObject(true)) {
+						TESObjectWEAP* wep = ((TESObjectWEAP*)(Actor*)(*g_thePlayer)->GetEquippedObject(true));
+						int weptype = wep->type();
+						ModifyAttackSpeedByTypes(wep, weptype, false);
+					}
 				}
 			}
 		}
 
 		//If the event is AttackWinStart or AttackWinStartLeft
 		else if (event == s_post || event == s_lpost) {
-			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_Post] + speedValues[ConfigType::Speed_Offset]);
-			ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_Post] + speedValues[ConfigType::Speed_LeftOffset]);
-			//_MESSAGE("post %f", speedValues[ConfigType::Speed_Post]);
+			if (speedAdjustmentEnabled) {
+				ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "WeaponSpeedMult", speedValues[ConfigType::Speed_Post] + speedValues[ConfigType::Speed_Offset]);
+				ActorModifier::SetCurrentAV((Actor*)(*g_thePlayer), "LeftWeaponSpeedMult", speedValues[ConfigType::Speed_Post] + speedValues[ConfigType::Speed_LeftOffset]);
+				//_MESSAGE("post %f", speedValues[ConfigType::Speed_Post]);
+			}
 
 			ActorModifier::RestrainMovement((Actor*)(*g_thePlayer), false);
 			ActorModifier::UnlockAim();
@@ -205,18 +260,39 @@ namespace BingleImmersiveImpact {
 				}
 			}
 		}
-		else if (configtype == ConfigType::RestrainMovement) {
+		else if (configtype == ConfigType::RestrainMovement)
 			ActorModifier::EnableRestraint(v);
-		}
-		else if (configtype == ConfigType::AimHelper) {
+
+		else if (configtype == ConfigType::AimHelper)
 			ActorModifier::EnableAimHelper(v);
-		}
-		else if (configtype == ConfigType::ChargeMul) {
+
+		else if (configtype == ConfigType::ChargeMul)
 			chargeMul = v;
-		}
-		else if (configtype == ConfigType::HitFeedback) {
+
+		else if (configtype == ConfigType::HitFeedback)
 			HitFeedback::EnableFeedback(v);
-		}
+
+		else if (configtype == ConfigType::SpeedAdjustment)
+			speedAdjustmentEnabled = v;
+
+		else if (configtype == ConfigType::DeflectChanceMul)
+			HitFeedback::deflectChanceMul = v;
+
+		else if (configtype == ConfigType::DeflectChanceMax)
+			HitFeedback::deflectChanceMax = v;
+
+		else if (configtype == ConfigType::StaggerResetTime)
+			HitFeedback::staggerResetTime = v;
+
+		else if (configtype == ConfigType::StaggerLimit)
+			HitFeedback::staggerLimit = v;
+
+		else if (configtype == ConfigType::StaggerDamageMax)
+			HitFeedback::staggerDamageMax = v;
+
+		else if (configtype == ConfigType::StaggerAny)
+			HitFeedback::staggerAny = v;
+
 		ConfigHandler::SetConfig(formId, (ConfigType)configtype, v);
 	}
 
@@ -228,11 +304,6 @@ namespace BingleImmersiveImpact {
 		UpdateConfig(base, formId, configtype, v);
 		SaveConfig(base);
 	}
-
-	void UpdateFromConfig(ConfigType c, float v) {
-		speedValues[c] = v;
-	}
-
 #pragma endregion
 }
 
