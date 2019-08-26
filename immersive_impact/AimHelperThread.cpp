5a312fd4c3a818a17dce5c86e3baaddbaebe3fd2
jarari <hazardhax11@gmail.com>
2019-08-26 20:13:26 +0900

    Better thread management

--- immersive_impact/AimHelperThread.cpp
+++ immersive_impact/AimHelperThread.cpp
@@ -5,14 +5,15 @@ bool AimHelperThread::isRunning = false;
 std::thread AimHelperThread::_t;
 
 void AimHelperThread::threadFunc(fn<void, Actor*, float> func, Actor* target, float size) {
-	while (isRunning) {
+	bool brake = false;
+	while (isRunning && !brake) {
 		if (target != nullptr) {
 			//Prevent execution on player killmove, dead targets
 			if (!(*g_thePlayer)->flags2.killMove && target->formType == kFormType_Character && !target->IsDead(1)) {
 				func(target, size);
 			}
 			else {
-				isRunning = false;
+				brake = true;
 				BingleEventInvoker::TranslateToTarget(*g_thePlayer);
 				BingleEventInvoker::StopTranslation();
 			}
@@ -22,7 +23,7 @@ void AimHelperThread::threadFunc(fn<void, Actor*, float> func, Actor* target, fl
 }
 
 void AimHelperThread::RequestThread(fn<void, Actor*, float> func, Actor* target, float size) {
-	if (_t.joinable()) {
+	if (isRunning) {
 		StopThread();
 	}
 	_t = std::thread(&threadFunc, func, target, size);
@@ -37,7 +38,7 @@ void AimHelperThread::StartThread() {
 
 void AimHelperThread::StopThread() {
 	isRunning = false;
-	if (_t.joinable()) {
+	if (_t.joinable() && isRunning) {
 		_t.join();
 		_t.std::thread::~thread();
 	}
