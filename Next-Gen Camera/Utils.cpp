05b1728c844430099fc27a150a351637c936d01b
jarari <hazardhax11@gmail.com>
2019-09-02 00:09:50 +0900

    Added functions for dialogue cam

--- Next-Gen Camera/Utils.cpp
+++ Next-Gen Camera/Utils.cpp
@@ -27,13 +27,13 @@ void Utils::NormalizeVector(NiPoint3& vec) {
 	vec.z /= scale;
 }
 
-void Utils::GetCameraForward(TESCamera* cam, NiPoint3* vecnorm) {
+void Utils::GetCameraForward(TESCamera* cam, NiPoint3& vecnorm) {
 	NiPoint3 fwd(0, 1, 0);
 	fwd = cam->cameraNode->m_worldTransform.rot * fwd;
 	NormalizeVector(fwd);
-	vecnorm->x = fwd.x;
-	vecnorm->y = fwd.y;
-	vecnorm->z = fwd.z;
+	vecnorm.x = fwd.x;
+	vecnorm.y = fwd.y;
+	vecnorm.z = fwd.z;
 }
 
 void Utils::SetMatrix33(float a, float b, float c, float d, float e, float f, float g, float h, float i, NiMatrix33& mat) {
@@ -65,6 +65,15 @@ NiMatrix33 Utils::GetRotationMatrix33(float pitch, float yaw, float roll) {
 				-sin(pitch),	0,					cos(pitch),
 				m_pitch);
 	return m_yaw * m_pitch * m_roll;
+}
+
+NiMatrix33 Utils::GetRotationMatrix33(NiPoint3 axis, float angle) {
+	float x = axis.x * sin(angle / 2.0f);
+	float y = axis.y * sin(angle / 2.0f);
+	float z = axis.z * sin(angle / 2.0f);
+	float w = cos(angle / 2.0f);
+	Quaternion q = Quaternion(x, y, z, w);
+	return q.ToRotationMatrix33();
 }
 
 //Sarrus rule
@@ -116,6 +125,61 @@ NiPoint3 Utils::Lerp(NiPoint3 start, NiPoint3 end, float t) {
 	return start + (end - start) * min(max(t, 0), 1);
 }
 
+void Utils::VecToAng(NiPoint3 vec, float& pitch, float& yaw) {
+	yaw = atan2(vec.y, vec.x);
+	pitch = -asin(vec.z);
+}
+
+//By himika
+bool Utils::GetNodePosition(TESObjectREFR* ref, const char* nodeName, NiPoint3& point) {
+	bool bResult = false;
+
+	if (nodeName[0])
+	{
+		NiAVObject* object = (NiAVObject*)ref->GetNiNode();
+		if (object)
+		{
+			object = object->GetObjectByName(&nodeName);
+			if (object)
+			{
+				point.x = object->m_worldTransform.pos.x;
+				point.y = object->m_worldTransform.pos.y;
+				point.z = object->m_worldTransform.pos.z;
+				bResult = true;
+			}
+		}
+	}
+
+	return bResult;
+}
+
+//By himika, modified for head
+bool Utils::GetHeadPos(TESObjectREFR* ref, NiPoint3& point) {
+	if (!ref->GetNiNode())
+		return false;
+
+	if (ref->formType == kFormType_Character) {
+		bool dataFound = false;
+		TESRace* race = ((Actor*)ref)->race;
+		BGSBodyPartData* bodyPart = race->bodyPartData;
+		BGSBodyPartData::Data* data = bodyPart->part[1];
+		if (data) {
+			if (GetNodePosition(ref, data->unk04.data, point)) {
+				dataFound = true;
+			}
+		}
+		if (!dataFound) {
+			ref->GetMarkerPosition(&point);
+		}
+	}
+	else {
+		point.x = ref->pos.x;
+		point.y = ref->pos.y;
+		point.z = ref->pos.z;
+	}
+	return true;
+}
+
 ActiveEffect* Utils::GetActiveEffectFromActor(Actor* actor) {
 	if (!actor || !actor->GetNiNode())
 		return nullptr;
@@ -123,7 +187,7 @@ ActiveEffect* Utils::GetActiveEffectFromActor(Actor* actor) {
 		it = storedActiveEffects.begin();
 		it != storedActiveEffects.end();) {
 		pair<Actor*, ActiveEffect*> p = *it;
-		if (p.first != nullptr && p.first->IsDead(1) == false && p.second->reference == (TESObjectREFR*)&p.first->magicTarget) {
+		if (p.first && p.second && p.first->IsDead(1) == false && p.second->reference == (TESObjectREFR*)&p.first->magicTarget) {
 			if (p.first == actor) {
 				_MESSAGE("Got ActiveEffect from stored data.");
 				return p.second;
@@ -153,4 +217,47 @@ ActiveEffect* Utils::GetActiveEffectFromActor(Actor* actor) {
 
 bool Utils::IsInMenuMode() {
 	return *((int*)0x1B3E428) > 0;
+}
+
+Quaternion::Quaternion(float _x, float _y, float _z, float _w) {
+	x = _x;
+	y = _y;
+	z = _z;
+	w = _w;
+}
+
+float Quaternion::Norm() {
+	return x * x + y * y + z * z + w * w;
+}
+
+//From https://android.googlesource.com/platform/external/jmonkeyengine/+/59b2e6871c65f58fdad78cd7229c292f6a177578/engine/src/core/com/jme3/math/Quaternion.java
+NiMatrix33 Quaternion::ToRotationMatrix33() {
+	float norm = Norm();
+	// we explicitly test norm against one here, saving a division
+	// at the cost of a test and branch.  Is it worth it?
+	float s = (norm == 1.0f) ? 2.0f : (norm > 0.0f) ? 2.0f / norm : 0;
+
+	// compute xs/ys/zs first to save 6 multiplications, since xs/ys/zs
+	// will be used 2-4 times each.
+	float xs = x * s;
+	float ys = y * s;
+	float zs = z * s;
+	float xx = x * xs;
+	float xy = x * ys;
+	float xz = x * zs;
+	float xw = w * xs;
+	float yy = y * ys;
+	float yz = y * zs;
+	float yw = w * ys;
+	float zz = z * zs;
+	float zw = w * zs;
+
+	// using s=2/norm (instead of 1/norm) saves 9 multiplications by 2 here
+	NiMatrix33 mat;
+	Utils::SetMatrix33(	1 - (yy + zz),		(xy - zw),			(xz + yw),
+						(xy + zw),			1 - (xx + zz),		(yz - xw),
+						(xz - yw),			(yz + xw),			1 - (xx + yy), 
+						mat);
+
+	return mat;
 }
--- Next-Gen Camera/Utils.h
+++ Next-Gen Camera/Utils.h
@@ -5,18 +5,30 @@ class NiMatrix33;
 class NiPoint3;
 class TESCamera;
 class ActiveEffect;
+class TESObjectREFR;
+struct Quaternion {
+public:
+	float x, y, z, w;
+	Quaternion(float _x, float _y, float _z, float _w);
+	float Norm();
+	NiMatrix33 ToRotationMatrix33();
+};
 namespace Utils {
 	float Scale(NiPoint3 vec);
 	void NormalizeVector(float& x, float& y, float& z);
 	void NormalizeVector(NiPoint3& vec);
-	void GetCameraForward(TESCamera* cam, NiPoint3* vecnorm);
+	void GetCameraForward(TESCamera* cam, NiPoint3& vecnorm);
 	void SetMatrix33(float a, float b, float c, float d, float e, float f, float g, float h, float i, NiMatrix33& mat);
 	NiMatrix33 GetRotationMatrix33(float pitch, float yaw, float roll);
+	NiMatrix33 GetRotationMatrix33(NiPoint3 axis, float angle);
 	float Determinant(NiMatrix33 mat);
 	NiMatrix33 Inverse(NiMatrix33 mat);
 	NiPoint3 WorldToLocal(NiPoint3 wpos, NiPoint3 lorigin, NiMatrix33 rot);
 	NiPoint3 LocalToWorld(NiPoint3 lpos, NiPoint3 lorigin, NiMatrix33 rot);
 	NiPoint3 Lerp(NiPoint3 start, NiPoint3 end, float t);
+	void VecToAng(NiPoint3 vec, float& pitch, float& yaw);
+	bool GetNodePosition(TESObjectREFR* ref, const char* nodeName, NiPoint3& point);
+	bool GetHeadPos(TESObjectREFR* ref, NiPoint3& point);
 	ActiveEffect* GetActiveEffectFromActor(Actor* actor);
 	bool IsInMenuMode();
 }
